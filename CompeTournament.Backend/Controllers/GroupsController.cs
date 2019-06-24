764dc1957a8b3121e814dd2ef39ea8cf7dcbdbac
Starling Germosen <sgermosen@outlook.com>
2019-06-24 12:52:47 -0400

    Merging Group With Tournament

--- CompeTournament.Backend/Controllers/GroupsController.cs
+++ CompeTournament.Backend/Controllers/GroupsController.cs
@@ -1,6 +1,8 @@
 ﻿using CompeTournament.Backend.Data.Entities;
 using CompeTournament.Backend.Persistence.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CompeTournament.Backend.Controllers
@@ -9,18 +11,35 @@ namespace CompeTournament.Backend.Controllers
     {
         private readonly IGroupRepository _groupRepo;
 
-        public GroupsController(IGroupRepository groupRepo)
+        public readonly ITournamentTypeRepository _typeRepository;
+
+        public GroupsController(IGroupRepository groupRepo, ITournamentTypeRepository typeRepository)
         {
             _groupRepo = groupRepo;
+            _typeRepository = typeRepository;
         }
         public async Task<IActionResult> Index()
         {
-            var groups =  _groupRepo.GetAll();
+            var groups = _groupRepo.GetWithType();
             return View(groups);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await _groupRepo.FindByIdAsync(id);
+
+            return View(model);
         }
 
         public async Task<IActionResult> Create()
         {
+            var tt = _typeRepository.GetAll().ToList();
+           // ViewBag.TournamentTypes = tt.ToList();
+            ViewData["TournamentTypeId"] = new SelectList(tt, "Id", "Name");
+            //var model = new GroupView
+            //{
+            //    TournamentTypes = tt.ToList()
+            //};
             return View();
         }
 
