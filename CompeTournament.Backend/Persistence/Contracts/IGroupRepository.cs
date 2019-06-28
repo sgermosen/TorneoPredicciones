47f88e9b5f8c9f5210090fe802d8097869e44014
Starling Germosen <sgermosen@outlook.com>
2019-06-28 10:55:00 -0400

    Accept a member on the group

--- CompeTournament.Backend/Persistence/Contracts/IGroupRepository.cs
+++ CompeTournament.Backend/Persistence/Contracts/IGroupRepository.cs
@@ -6,9 +6,18 @@ namespace CompeTournament.Backend.Persistence.Contracts
 {
     public interface IGroupRepository : IRepository<Group>
     {
+        Task<GroupUser> GroupMember(int key);
+
+        IQueryable<GroupUser> GroupMembers(int id);
+
         IQueryable<Group> GetWithType();
+
         Task<Group> GetByIdWithChildrens(int id);
+
        // Task<bool> JoinRequest(GroupUser entity);
+
         Task<GroupUser> JoinRequest(GroupUser entity);
+
+        Task<GroupUser> AcceptRequestJoin(GroupUser entity);
     }
 }
