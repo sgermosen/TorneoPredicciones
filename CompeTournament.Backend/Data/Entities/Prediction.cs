39496f3f4ce126295a8c018c5f48167ce2068bf2
Starling Germosen <sgermosen@outlook.com>
2019-06-30 20:02:16 -0400

    creacion de la prediccion

---
+++ CompeTournament.Backend/Data/Entities/Prediction.cs
@@ -0,0 +1,31 @@
+﻿using CompeTournament.Backend.Helpers;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompeTournament.Backend.Data.Entities
+{
+    public class Prediction : AuditEntity, IBaseEntity
+    {
+        //[StringLength(450)]
+        //public string ApplicationUserId { get; set; }
+        //public ApplicationUser ApplicationUser { get; set; }
+
+        public int MatchId { get; set; }
+        [ForeignKey("MatchId")]
+        public Match Match { get; set; }
+
+        public int? LocalPoints { get; set; }
+
+        public int? VisitorPoints { get; set; }
+
+        public int? Position { get; set; }
+
+        public int AdquiredPoints { get; set; }
+
+        //public int GroupId { get; set; }
+        //[ForeignKey("GroupId")]
+        //public Group Group { get; set; }
+
+
+    }
+}
--- CompeTournament.Backend/Migrations/20190630233749_PredictionRemove.Designer.cs
+++ CompeTournament.Backend/Migrations/20190630233749_PredictionRemove.Designer.cs
@@ -4,14 +4,16 @@ using CompeTournament.Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CompeTournament.Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190630233749_PredictionRemove")]
+    partial class PredictionRemove
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
